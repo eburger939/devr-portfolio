e0448051970fc5d1947903717730b4be35ecf67c
Emily Burger <eburger939@gmail.com>
2022-02-15 20:54:28 -0600

    added about section with some css styling

---
+++ dev-port/src/components/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function About() {
+    return (
+        <section className="page-section mb-0" id="about">
+            <div className="container">
+                <h2 className="page-section-heading text-center text-uppercase">About</h2>
+            
+                    <div className="row">
+                        <div className="col-lg-6 ms-auto"><p className="lead"><strong>Full stack web developer with a background in healthcare as a Board Certified Psychiatric Clinical Pharmacy Specialist.  A few of my newly developed technical skills include HTML, CSS, Javascript, Node.js, Express, and MySQL, Sequelize, MongoDB and React.</strong></p></div>
+                        <div className="col-lg-6 me-auto"><p className="lead"><strong>I am extremely passionate about learning and expanding my knowledge of the tech world!  My acute attention to detail and relentless problem-solving abilities enable me to create meaningful, unique digital platforms.  My goal always is to improve efficiency!</strong></p></div>
+
+                    </div>
+
+                </div>
+                </section>
+                );
+}
