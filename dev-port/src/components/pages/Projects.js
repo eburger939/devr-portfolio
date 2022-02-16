05885acdce56bb75c2014025d1e8965e347503cb
Emily Burger <eburger939@gmail.com>
2022-02-15 21:36:09 -0600

    added generic modal to project page

--- dev-port/src/components/pages/Projects.js
+++ dev-port/src/components/pages/Projects.js
@@ -1,4 +1,9 @@
 import React from 'react';
+import Modal from "react-bootstrap/Modal";
+import ModalBody from "react-bootstrap/ModalBody";
+import ModalHeader from "react-bootstrap/ModalHeader";
+import ModalFooter from "react-bootstrap/ModalFooter";
+import ModalTitle from "react-bootstrap/ModalTitle";
 
 export default function Projects() {
     return (
@@ -69,6 +74,18 @@ export default function Projects() {
                     </div>
                     </div>
 
+                    <Modal show={true}>
+      <ModalHeader>
+        <ModalTitle>Hi</ModalTitle>
+      </ModalHeader>
+      <ModalBody>asdfasdf</ModalBody>
+      <ModalFooter>This is the footer</ModalFooter>
+    </Modal>
+
         </section>
+
+
+
+
     );
 }
