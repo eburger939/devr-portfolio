d4c679e6b9b5c2da7b56987d18054732d231c54c
Emily Burger <eburger939@gmail.com>
2022-02-15 21:04:19 -0600

    added projects section with pictures of projects

---
+++ dev-port/src/components/pages/Projects.js
@@ -0,0 +1,74 @@
+import React from 'react';
+
+export default function Projects() {
+    return (
+        <section className="page-section portfolio" id="portfolio">
+            <div className="container">
+                <h2 className="page-section-heading text-white text-center text-uppercase text-secondary mb-0">Portfolio</h2>
+
+
+                <div className="row justify-content-center">
+
+                    <div className="col-md-6 col-lg-4 mb-5">
+                        <div className="portfolio-item mx-auto port-item">
+                            <div className="portfolio-item mx-auto" data-bs-toggle="modal" data-bs-target="#portfolioModal1">
+                                <div className="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
+                                    <div className="portfolio-item-caption-content text-center text-white"><i className="fas fa-plus fa-3x"></i></div>
+                                </div>
+                                <img className="img-fluid" src="images/petrec3.png" alt="..." />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-6 col-lg-4 mb-5">
+                        <div className="portfolio-item mx-auto port-item">
+                         <div className="portfolio-item mx-auto" data-bs-toggle="modal" data-bs-target="#portfolioModal2">
+                            <div className="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
+                                <div className="portfolio-item-caption-content text-center text-white"><i className="fas fa-plus fa-3x"></i></div>
+                            </div> 
+                           
+                                <img className="img-fluid" src="/images/movieroulette.png" alt="..." />
+                         
+                         </div>
+                        </div>
+                    </div>
+                    <div className="col-md-6 col-lg-4 mb-5">
+                        <div className="portfolio-item mx-auto port-item">
+                        <div className="portfolio-item mx-auto" data-bs-toggle="modal" data-bs-target="#portfolioModal3">
+                            <div className="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
+                                <div className="portfolio-item-caption-content text-center text-white"><i className="fas fa-plus fa-3x"></i></div>
+                            </div>
+                            
+                                <img className="img-fluid" src="images/advblog1.png" alt="..." />
+                       </div>
+                        </div> 
+                    </div> 
+
+                    <div className="col-md-6 col-lg-4 mb-5 mb-md-0">
+                        <div className="portfolio-item mx-auto port-item">
+                        <div className="portfolio-item mx-auto" data-bs-toggle="modal" data-bs-target="#portfolioModal4"> 
+                             <div className="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
+                                <div className="portfolio-item-caption-content text-center text-white"><i className="fas fa-plus fa-3x"></i></div>
+                            </div>
+                      
+                                <img className="img-fluid" src="images/weather.png" alt="..." />
+                        </div>
+                    </div>
+                    </div>
+                    <div className="col-md-6 col-lg-4 mb-5 mb-md-0">
+                        <div className="portfolio-item mx-auto port-item">
+                        <div className="portfolio-item mx-auto" data-bs-toggle="modal" data-bs-target="#portfolioModal5">
+                            <div className="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
+                                <div className="portfolio-item-caption-content text-center text-white"><i className="fas fa-plus fa-3x"></i></div>
+                            </div>
+                            <img className="img-fluid" src="images/thwider1.png" alt="..." />
+                        </div>
+                    </div>
+                    </div>
+
+
+                    </div>
+                    </div>
+
+        </section>
+    );
+}
