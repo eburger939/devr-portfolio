7c0d2a6f94d371ff719168323ed612072ab059f2
Emily Burger <eburger939@gmail.com>
2022-02-15 22:30:14 -0600

    modal working for first project

--- dev-port/src/components/pages/Projects.js
+++ dev-port/src/components/pages/Projects.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import { React, useState, useEffect } from 'react';
 import Modal from "react-bootstrap/Modal";
 import ModalBody from "react-bootstrap/ModalBody";
 import ModalHeader from "react-bootstrap/ModalHeader";
@@ -6,6 +6,16 @@ import ModalFooter from "react-bootstrap/ModalFooter";
 import ModalTitle from "react-bootstrap/ModalTitle";
 
 export default function Projects() {
+    const [isOpen, setIsOpen] = useState(false);
+    const showModal = () => {
+        setIsOpen(true)
+    };
+    const hideModal = () => {
+        setIsOpen(false)
+    };
+
+
+
     return (
         <section className="page-section portfolio" id="portfolio">
             <div className="container">
@@ -14,9 +24,9 @@ export default function Projects() {
 
                 <div className="row justify-content-center projects-row">
 
-                    <div className="col-md-6 col-lg-4 mb-5">
+                    <div className="col-md-6 col-lg-4 mb-5" onClick={showModal}>
                         <div className="portfolio-item mx-auto port-item">
-                            <div className="portfolio-item mx-auto" data-bs-toggle="modal" data-bs-target="#portfolioModal1">
+                            <div className="portfolio-item mx-auto" data-bs-toggle={"modal"} data-bs-target="#portfolioModal1">
                                 <div className="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
                                     <div className="portfolio-item-caption-content text-center text-white"><i className="fas fa-plus fa-3x"></i></div>
                                 </div>
@@ -26,61 +36,85 @@ export default function Projects() {
                     </div>
                     <div className="col-md-6 col-lg-4 mb-5">
                         <div className="portfolio-item mx-auto port-item">
-                         <div className="portfolio-item mx-auto" data-bs-toggle="modal" data-bs-target="#portfolioModal2">
-                            <div className="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
-                                <div className="portfolio-item-caption-content text-center text-white"><i className="fas fa-plus fa-3x"></i></div>
-                            </div> 
-                           
+                            <div className="portfolio-item mx-auto" data-bs-toggle="modal" data-bs-target="#portfolioModal2">
+                                <div className="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
+                                    <div className="portfolio-item-caption-content text-center text-white"><i className="fas fa-plus fa-3x"></i></div>
+                                </div>
+
                                 <img className="img-fluid" src="/images/movieroulette.png" alt="..." />
-                         
-                         </div>
+
+                            </div>
                         </div>
                     </div>
                     <div className="col-md-6 col-lg-4 mb-5">
                         <div className="portfolio-item mx-auto port-item">
-                        <div className="portfolio-item mx-auto" data-bs-toggle="modal" data-bs-target="#portfolioModal3">
-                            <div className="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
-                                <div className="portfolio-item-caption-content text-center text-white"><i className="fas fa-plus fa-3x"></i></div>
-                            </div>
-                            
+                            <div className="portfolio-item mx-auto" data-bs-toggle="modal" data-bs-target="#portfolioModal3">
+                                <div className="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
+                                    <div className="portfolio-item-caption-content text-center text-white"><i className="fas fa-plus fa-3x"></i></div>
+                                </div>
+
                                 <img className="img-fluid" src="images/advblog1.png" alt="..." />
-                       </div>
-                        </div> 
-                    </div> 
+                            </div>
+                        </div>
+                    </div>
 
                     <div className="col-md-6 col-lg-4 mb-5 mb-md-0">
                         <div className="portfolio-item mx-auto port-item">
-                        <div className="portfolio-item mx-auto" data-bs-toggle="modal" data-bs-target="#portfolioModal4"> 
-                             <div className="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
-                                <div className="portfolio-item-caption-content text-center text-white"><i className="fas fa-plus fa-3x"></i></div>
-                            </div>
-                      
+                            <div className="portfolio-item mx-auto" data-bs-toggle="modal" data-bs-target="#portfolioModal4">
+                                <div className="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
+                                    <div className="portfolio-item-caption-content text-center text-white"><i className="fas fa-plus fa-3x"></i></div>
+                                </div>
+
                                 <img className="img-fluid" src="images/weather.png" alt="..." />
+                            </div>
                         </div>
-                    </div>
                     </div>
                     <div className="col-md-6 col-lg-4 mb-5 mb-md-0">
                         <div className="portfolio-item mx-auto port-item">
-                        <div className="portfolio-item mx-auto" data-bs-toggle="modal" data-bs-target="#portfolioModal5">
-                            <div className="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
-                                <div className="portfolio-item-caption-content text-center text-white"><i className="fas fa-plus fa-3x"></i></div>
+                            <div className="portfolio-item mx-auto" data-bs-toggle="modal" data-bs-target="#portfolioModal5">
+                                <div className="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
+                                    <div className="portfolio-item-caption-content text-center text-white"><i className="fas fa-plus fa-3x"></i></div>
+                                </div>
+                                <img className="img-fluid" src="images/thwider1.png" alt="..." />
                             </div>
-                            <img className="img-fluid" src="images/thwider1.png" alt="..." />
                         </div>
                     </div>
-                    </div>
 
 
-                    </div>
-                    </div>
+                </div>
+            </div>
 
-                    <Modal show={true}>
-      <ModalHeader>
-        <ModalTitle>Hi</ModalTitle>
-      </ModalHeader>
-      <ModalBody>asdfasdf</ModalBody>
-      <ModalFooter>This is the footer</ModalFooter>
-    </Modal>
+            <Modal
+                show={isOpen}
+                onHide={hideModal}
+                size='lg'
+                aria-labelledby="contained-modal-title-vcenter"
+                centered>
+
+
+                <div className="modal-header border-0"><button className="btn-close" type="button" onClick={hideModal} data-bs-dismiss="modal" aria-label="Close"></button></div>
+                <div className="modal-body  pb-1">
+                    <div className="container">
+                        <div className="row ">
+                            <div className="col">
+                                <h2 className="portfolio-modal-title text-secondary text-center text-uppercase mb-0">petRec <i className="fas fa-star-of-life custom-star"></i></h2>
+
+                                <a href="https://pet-rec.herokuapp.com/" target="_blank"> <img className="img-fluid rounded mb-1" src="images/petrec2.png" alt="..." /></a>
+                                <p className="mb-4">Manage and maintain all your pet records, medications, and vaccinations in one application!  PetRec is a tool for all pet owers to assist in organizing their pets medications, vaccinations, and health records.  A profile, or chart, can be create for every pet in the househould, and individualized based on that pets' health needs!  No more papers, binders, or forgetting when your pup's next monthly heartworm medication is due!</p>
+                                <p>Technologies Used: HTML, CSS, Javascript, Node.js, Express.js, Bcrypt, Handlebars, MySQL (Sequelize), Multer </p>
+                                <div className='links text-center'>
+                                    <a href="https://pet-rec.herokuapp.com/" target="_blank">Deployed Application</a>
+                                    <br></br>
+
+                                    <a href="https://github.com/eburger939/pet-ehr" target="_blank">GitHub Repository</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+            </Modal>
 
         </section>
 
