a630b9d4a14c4645d2814efb3b4895fa4945ef36
Emily Burger <eburger939@gmail.com>
2022-02-16 20:26:12 -0600

    added object variable with project information

--- dev-port/src/components/PortfolioContainer.js
+++ dev-port/src/components/PortfolioContainer.js
@@ -8,7 +8,56 @@ import Footer from './pages/Footer'
 
 import './PC.css'
 
-export default function PortfolioContainer() {
+const projs = [
+  {
+    mainImage: "images/petrec3.png",
+    description: "Manage and maintain all your pet records, medications, and vaccinations in one application!  PetRec is a tool for all pet owers to assist in organizing their pets medications, vaccinations, and health records.  A profile, or chart, can be create for every pet in the househould, and individualized based on that pets' health needs!  No more papers, binders, or forgetting when your pup's next monthly heartworm medication is due!",
+    technologies: "Technologies Used: HTML, CSS, Javascript, Node.js, Express.js, Bcrypt, Handlebars, MySQL (Sequelize), Multer",
+    modalImage: "images/petrec2.png",
+    href: "https://pet-rec.herokuapp.com/",
+    gitHub: "https://github.com/eburger939/pet-ehr",
+    title: "PETREC"
+  },
+  {
+    mainImage: "/images/movieroulette.png",
+    description: "If you have ever been indecisive on what to watch on your preferred streaming service, let Movie Roulette assist you!  Choose your genre and streaming service then spin the wheel!  You will be presented with six random suggestion for your next movie night.  Then click on the movie of choice to find more information!",
+    technologies: "Technologies Used: HTML, CSS, Javascript, Bulma, IMDB API, Streaming Services API",
+    modalImage: "images/mr2.png",
+    href: "https://eburger939.github.io/Movie-Roulette/",
+    gitHub: "https://github.com/eburger939/Movie-Roulette",
+    title: "Movie Roulette"
+  },
+  {
+    mainImage: "images/advblog1.png",
+    description: "Adventurer's blog is a website meant for explorers!  Create posts on your greatest adventures, your plans for travel and more!  As a user you can create new blogs and edit or delete your previous posts!  Users can comment on each other's posts to communicate ideas and experiences!",
+    technologies: "Technologies Used: HTML, CSS, Javascript, Node.js, Express.js, Bcrypt, Handlebars, MySQL (Sequelize)",
+    modalImage: "images/adv-blog2.png",
+    href: "https://adv-blog.herokuapp.com/",
+    gitHub: "https://github.com/eburger939/adventurers-blog",
+    title: "Adventurer's Blog"
+  },
+  {
+    mainImage: "images/weather.png",
+    description: "Interested in knowing the 5 day forecast in your favorite city?  Check out this weather dashboard!  Users can see the current weather, and weather for the next five days!  Search results save on the left for easier access!",
+    technologies: "Technologies Used: HTML, CSS, Javascript, Open Weather Map API",
+    modalImage: "images/weatherdash1.png",
+    href: "https://eburger939.github.io/weather-dashboard/",
+    gitHub: "https://github.com/eburger939/weather-dashboard",
+    title: "Weather Dashboard"
+  },
+  {
+    mainImage: "images/thwider1.png",
+    description: "Manage and maintain all your pet records, medications, and vaccinations in one application!  PetRec is a tool for all pet owers to assist in organizing their pets medications, vaccinations, and health records.  A profile, or chart, can be create for every pet in the househould, and individualized based on that pets' health needs!  No more papers, binders, or forgetting when your pup's next monthly heartworm medication is due!",
+    technologies: "Technologies Used: HTML, CSS, Javascript, Node.js, Express.js, Bcrypt, Handlebars, MySQL (Sequelize), Multer",
+    modalImage: "https://drive.google.com/file/d/13g6wy2xKoHOteDt9ejRnUupzkiZb1blv/preview",
+    href: "https://drive.google.com/file/d/13g6wy2xKoHOteDt9ejRnUupzkiZb1blv/preview",
+    gitHub: "https://github.com/eburger939/team-profile-generator",
+    title: "Team Generator"
+  },
+]
+
+
+function PortfolioContainer() {
   const [currentPage, setCurrentPage] = useState('Home');
 
   const renderPage = () => {
@@ -19,7 +68,7 @@ export default function PortfolioContainer() {
       return <About />;
     }
     if (currentPage === 'Projects') {
-      return <Projects />;
+      return <Projects projs={projs}/>;
     }
     return <Contact />;
   };
@@ -35,3 +84,5 @@ export default function PortfolioContainer() {
     </div>
   );
 }
+
+export default PortfolioContainer;
--- dev-port/src/components/pages/Projects.js
+++ dev-port/src/components/pages/Projects.js
@@ -5,7 +5,7 @@ import ModalHeader from "react-bootstrap/ModalHeader";
 import ModalFooter from "react-bootstrap/ModalFooter";
 import ModalTitle from "react-bootstrap/ModalTitle";
 
-export default function Projects() {
+export default function Projects(props) {
     const [isOpen, setIsOpen] = useState(false);
     const showModal = () => {
         setIsOpen(true)
@@ -19,102 +19,75 @@ export default function Projects() {
     return (
         <section className="page-section portfolio" id="portfolio">
             <div className="container">
+                
                 <h2 className="page-section-heading text-black text-center text-uppercase text-secondary mb-0">Projects</h2>
 
-
+              
                 <div className="row justify-content-center projects-row">
+                {props.projs.map((project) => (
 
-                    <div className="col-md-6 col-lg-4 mb-5" onClick={showModal}>
-                        <div className="portfolio-item mx-auto port-item">
-                            <div className="portfolio-item mx-auto" data-bs-toggle={"modal"} data-bs-target="#portfolioModal1">
-                                <div className="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
-                                    <div className="portfolio-item-caption-content text-center text-white"><i className="fas fa-plus fa-3x"></i></div>
+                    <><div className="col-md-6 col-lg-4 mb-5" onClick={showModal} key={project.id}>
+                            <div className="portfolio-item mx-auto port-item">
+                                <div className="portfolio-item mx-auto" data-bs-toggle={"modal"} data-bs-target="#portfolioModal1">
+                                    <div className="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
+                                        <div className="portfolio-item-caption-content text-center text-white"><i className="fas fa-plus fa-3x"></i></div>
+                                    </div>
+                                    <img className="img-fluid" src={project.mainImage} alt="..." />
                                 </div>
-                                <img className="img-fluid" src="images/petrec3.png" alt="..." />
                             </div>
                         </div>
-                    </div>
-                    <div className="col-md-6 col-lg-4 mb-5">
-                        <div className="portfolio-item mx-auto port-item">
-                            <div className="portfolio-item mx-auto" data-bs-toggle="modal" data-bs-target="#portfolioModal2">
-                                <div className="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
-                                    <div className="portfolio-item-caption-content text-center text-white"><i className="fas fa-plus fa-3x"></i></div>
-                                </div>
+                                              <>
+                                             
+                                {/* <Modal
+                                    show={isOpen}
+                                    onHide={hideModal}
+                                    size='lg'
+                                    aria-labelledby="contained-modal-title-vcenter"
+                                    centered>
 
-                                <img className="img-fluid" src="/images/movieroulette.png" alt="..." />
 
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-md-6 col-lg-4 mb-5">
-                        <div className="portfolio-item mx-auto port-item">
-                            <div className="portfolio-item mx-auto" data-bs-toggle="modal" data-bs-target="#portfolioModal3">
-                                <div className="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
-                                    <div className="portfolio-item-caption-content text-center text-white"><i className="fas fa-plus fa-3x"></i></div>
-                                </div>
+                                    <div className="modal-header border-0"><button className="btn-close" type="button" onClick={hideModal} data-bs-dismiss="modal" aria-label="Close"></button></div>
+                                    <div className="modal-body  pb-1">
+                                        <div className="container">
+                                            <div className="row ">
+                                                <div className="col">
+                                                    <h2 className="portfolio-modal-title text-secondary text-center text-uppercase mb-0">{props.message} <i className="fas fa-star-of-life custom-star"></i></h2>
 
-                                <img className="img-fluid" src="images/advblog1.png" alt="..." />
-                            </div>
-                        </div>
-                    </div>
+                                                    <a href={project.href} target="_blank"> <img className="img-fluid rounded mb-1" src={project.modalImage} alt="..." /></a>
+                                                    <p className="mb-4">{project.description}</p>
+                                                    <p>{project.technologies} </p>
+                                                    <div className='links text-center'>
+                                                        <a href={project.href} target="_blank">Deployed Application</a>
+                                                        <br />
 
-                    <div className="col-md-6 col-lg-4 mb-5 mb-md-0">
-                        <div className="portfolio-item mx-auto port-item">
-                            <div className="portfolio-item mx-auto" data-bs-toggle="modal" data-bs-target="#portfolioModal4">
-                                <div className="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
-                                    <div className="portfolio-item-caption-content text-center text-white"><i className="fas fa-plus fa-3x"></i></div>
-                                </div>
+                                                        <a href={project.gitHub} target="_blank">GitHub Repository</a>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
 
-                                <img className="img-fluid" src="images/weather.png" alt="..." />
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-md-6 col-lg-4 mb-5 mb-md-0">
-                        <div className="portfolio-item mx-auto port-item">
-                            <div className="portfolio-item mx-auto" data-bs-toggle="modal" data-bs-target="#portfolioModal5">
-                                <div className="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
-                                    <div className="portfolio-item-caption-content text-center text-white"><i className="fas fa-plus fa-3x"></i></div>
-                                </div>
-                                <img className="img-fluid" src="images/thwider1.png" alt="..." />
-                            </div>
-                        </div>
-                    </div>
+
+                                </Modal> */}
+                            </></>             
+                    
+                    
+                    ))}
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                   
 
 
                 </div>
             </div>
 
-            <Modal
-                show={isOpen}
-                onHide={hideModal}
-                size='lg'
-                aria-labelledby="contained-modal-title-vcenter"
-                centered>
-
-
-                <div className="modal-header border-0"><button className="btn-close" type="button" onClick={hideModal} data-bs-dismiss="modal" aria-label="Close"></button></div>
-                <div className="modal-body  pb-1">
-                    <div className="container">
-                        <div className="row ">
-                            <div className="col">
-                                <h2 className="portfolio-modal-title text-secondary text-center text-uppercase mb-0">petRec <i className="fas fa-star-of-life custom-star"></i></h2>
-
-                                <a href="https://pet-rec.herokuapp.com/" target="_blank"> <img className="img-fluid rounded mb-1" src="images/petrec2.png" alt="..." /></a>
-                                <p className="mb-4">Manage and maintain all your pet records, medications, and vaccinations in one application!  PetRec is a tool for all pet owers to assist in organizing their pets medications, vaccinations, and health records.  A profile, or chart, can be create for every pet in the househould, and individualized based on that pets' health needs!  No more papers, binders, or forgetting when your pup's next monthly heartworm medication is due!</p>
-                                <p>Technologies Used: HTML, CSS, Javascript, Node.js, Express.js, Bcrypt, Handlebars, MySQL (Sequelize), Multer </p>
-                                <div className='links text-center'>
-                                    <a href="https://pet-rec.herokuapp.com/" target="_blank">Deployed Application</a>
-                                    <br></br>
-
-                                    <a href="https://github.com/eburger939/pet-ehr" target="_blank">GitHub Repository</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-
-            </Modal>
+ 
 
         </section>
 
